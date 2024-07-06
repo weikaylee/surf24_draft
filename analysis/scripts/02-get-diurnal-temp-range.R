4d20c9624d94b9fffe4cdf9a42fb5ce4fe55cfbd
Kaylee Wei <kaywei@caltech.edu>
2024-07-06 13:22:02 -0700

    get diurnal range rasters

---
+++ analysis/scripts/02-get-diurnal-temp-range.R
@@ -0,0 +1,103 @@
+# load libraries
+library(raster)
+
+# consts
+years <- c(2003:2020)
+
+# QUESTION: how was the atmin, atmax temp collected in the first place :000
+base_paths <- list(
+  min = file.path("analysis", "data", "la_atmin"),
+  max = file.path("analysis", "data", "la_atmax"),
+  rng = file.path("analysis", "processed", "la_dtrng")
+)
+
+# create list of vectors of temperature paths for each year
+yearly_data <- list() 
+for (year in years) {
+  yearly_data[[as.character(year)]] <- c(
+    MIN = file.path(base_paths[["min"]], paste("LA_ATMIN", year, sep="_")), 
+    MAX = file.path(base_paths[["max"]], paste("LA_ATMAX", year, sep="_")), 
+    RNG = file.path(base_paths[["rng"]], paste("LA_DTRNG", year, sep="_"))
+  )
+}
+
+# create dirs for range rasters
+for (year in yearly_data) {
+  path <- year[["RNG"]]
+  if (!dir.exists(path)) {
+    dir.create(path, recursive=TRUE)
+  }
+}
+
+# function to find file in file_list that matches doy of file_to_match
+# (search file_list linearly in case of large mismatches between dirs)
+get_matching_file <- function(file_list, file_to_match) {
+  doy_to_match <- regmatches(basename(file_to_match), gregexpr("DOY(//d+)", basename(file_to_match)))
+  for (i in 1:length(file_list)) {
+    file <- file_list[i]
+    file_doy <- regmatches(basename(file), gregexpr("DOY(//d+)", basename(file)))
+    if (identical(doy_to_match, file_doy)) {
+      return(file)
+    }
+  }
+  return(NULL)
+}
+
+# get and store range raster
+for (year in yearly_data) {
+  min_files <- list.files(year[["MIN"]], full.names=TRUE)
+  max_files <- list.files(year[["MAX"]], full.names=TRUE)
+  rng_path <- year[["RNG"]]
+  
+  print(paste0("Writing to ", rng_path, "..."))
+  for (i in 1:length(min_files)) { 
+    min_file = min_files[i]
+    max_file = get_matching_file(max_files, min_file) 
+    
+    if (is.null(max_file)) {
+      next
+    }
+    
+    filename <- sub("ATMIN", "DTRNG", basename(min_file))
+    filepath <- file.path(rng_path, filename)
+    
+    # calculate and save avg raster
+    min_raster <- raster(min_file)
+    max_raster <- raster(max_file)
+    avg_raster <- (max_raster - min_raster)
+    
+    # save raster
+    writeRaster(avg_raster, filepath, overwrite=TRUE)
+  }
+}
+
+# verify that calculation worked 
+# first, check that we have the expected number of files 
+# (minimum of max temp files and min temp files) 
+for (year in names(yearly_data)) {
+  year_paths <- yearly_data[[year]]
+  min_files <- list.files(year_paths[["MIN"]])
+  print(length(min_files))
+  max_files <- list.files(year_paths[["MAX"]])
+  rng_files <- list.files(year_paths[["RNG"]])
+  num_files <- min(length(min_files), length(max_files))
+  actual_files <- length(rng_files)
+  
+  if (num_files != actual_files) {
+    stop(paste("Number of files do not match in", year))
+  }
+}
+
+# check that raster info is consistent
+min_test <- raster(file.path("analysis", "data", "la_atmin", "LA_ATMIN_2004", "LA_ATMIN_2004_DOY001.tif"))
+max_test <- raster(file.path("analysis", "data", "la_atmax", "LA_ATMAX_2004", "LA_ATMAX_2004_DOY001.tif"))
+avg_test <- raster(file.path("analysis", "processed", "la_dtrng", "LA_DTRNG_2004", "LA_DTRNG_2004_DOY001.tif"))
+
+min_test <- setMinMax(min_test)
+max_test <- setMinMax(max_test)
+avg_test <- setMinMax(avg_test)
+
+print(min_test)
+print(max_test)
+print(avg_test)
+
