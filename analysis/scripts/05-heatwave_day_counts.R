35a66dda635727e4d4a595af9f8568d1d1c7a533
Kaylee Wei <kaywei@caltech.edu>
2024-07-10 16:55:23 -0700

    get heatwave threshold

--- analysis/scripts/05-heatwave_day_counts.R
+++ analysis/scripts/05-heatwave_day_counts.R
@@ -1,18 +1,35 @@
 # load libraries
 library(terra)
+library(sf)
 
 # consts
 years <- c(2003:2020)
 input_path <- file.path("analysis", "data", "la_atmin")
 output_path <- file.path("analysis", "processed", "heatwave_counts")
+# shp_file <- file.path("analysis", "data", "la_county_boundary", "County_Boundary.shp")
+shp_file <- file.path("analysis", "data", "la_census_shp", "LA_HRS_income.shp")
+threshold_file <- file.path("analysis", "data", "summer_85p.tif")
 
-threshold <- 22
+# read shp file
+la_shp <- read_sf(shp_file)
+la_geo <- vect(la_shp[["geometry"]]) # convert SpatVector to pass into terra's extract()
+
+# get threshold for la, by extracting from hannah's work (uses epa def of heatwave)
+thresholds <- rast(threshold_file)
+print("Getting heatwave threshold for LA county...")
+extracted_vals <- extract(thresholds, la_geo, weights=TRUE) # returns df, with temp of corresponding cells and the fraction of how much cell is covered
+sum_weights <- sum(extracted_vals$weight)
+extracted_vals$temp_weights <- extracted_vals$weight / sum_weights
+if (sum(extracted_vals$temp_weights) != 1) {
+  stop(paste("Sum of temp weights is not 1. Actual sum is", sum_temp_weights))
+}
+extracted_vals$weighted_temp <- extracted_vals$summer_85p * extracted_vals$temp_weights
+la_threshold <- sum(extracted_vals$weighted_temp, na.rm=TRUE) # relatively low, since using daily min temps (usually collected during nighttime)
 
 # create list of paths for input data
 yearly_data <- list()
 for (year in years) {
-  yearly_data[[as.character(year)]] <- file.path(input_path, paste("LA_ATMIN", year, sep =
-                                                                    "_"))
+  yearly_data[[as.character(year)]] <- file.path(input_path, paste("LA_ATMIN", year, sep = "_"))
 }
 
 # create output dir
@@ -25,13 +42,12 @@ get_heatwave_counts <- function(pixel_vec) {
   num_heatwaves <- 0
   is_unique_heatwave <- TRUE
   for (i in 1:(length(pixel_vec) - 1)) {
-    # TODO, should i do > or >= threshold?
     if (!is.nan(pixel_vec[i]) && !is.nan(pixel_vec[i+1])) {
-      if (is_unique_heatwave && (pixel_vec[i] >= threshold && pixel_vec[i+1] >= threshold)) {
+      if (is_unique_heatwave && (pixel_vec[i] > la_threshold && pixel_vec[i+1] > la_threshold)) {
         num_heatwaves <- num_heatwaves + 1
         is_unique_heatwave <- FALSE
       } 
-      else if (pixel_vec[i] < threshold) {
+      else if (pixel_vec[i] <= la_threshold) {
         is_unique_heatwave <- TRUE # reset
       }
     }
@@ -50,26 +66,34 @@ for (year in names(yearly_data)) {
   writeRaster(binned_raster, raster_path, overwrite = TRUE)
 }
 
-# create dummy raster to test alg
-ncols <- 2
-nrows <- 2
-# create layers
-test_raster_l1 <- rast(nrows = nrows, ncols = ncols)
-values(test_raster_l1) <- c(99, 99, 99, -99)
-test_raster_l2 <- rast(nrows = nrows, ncols = ncols)
-values(test_raster_l2) <- c(99, 99, 0, 99)
-test_raster_l3 <- rast(nrows = nrows, ncols = ncols)
-values(test_raster_l3) <- c(-99, 99, 0, -99)
-test_raster_l4 <- rast(nrows = nrows, ncols = ncols)
-values(test_raster_l4) <- c(99, 99, 99, 99)
-test_raster_l5 <- rast(nrows = nrows, ncols = ncols)
-values(test_raster_l5) <- c(99, 99, 99, -99)
+# plot multiple rasters, observe any trends 
+nrows = 3
+ncols = 4
+par(mfrow = c(nrows, ncols), mar = c(2, 2, 2, 2))
+sequence <- seq(from = 1, by = 3, length.out = 6)
+for (i in sequence) {
+  year <- years[i]
+  plot_raster_path <- file.path("analysis", "processed", "heatwave_counts", paste0("LA_HEATWAVE_CNT_", year, ".tif"))
+  plot_raster <- rast(plot_raster_path)
+  plot(plot_raster, main=basename(plot_raster_path))
+  hist(plot_raster, main=basename(plot_raster_path))
+}
 
-test_stack <- c(test_raster_l1, test_raster_l2, test_raster_l3, test_raster_l4, test_raster_l5)
-test_binned_raster <- app(test_stack, fun=get_heatwave_counts)
-plot(test_binned_raster) # matches expected: 2, 1, 1, 0
-
-# plot example raster 
-ex_raster <- rast(file.path("analysis", "processed", "heatwave_counts", "LA_HEATWAVE_CNT_2003.tif"))
-plot(ex_raster)
-
+# # create dummy raster to test alg
+# ncols <- 2
+# nrows <- 2
+# # create layers
+# test_raster_l1 <- rast(nrows = nrows, ncols = ncols)
+# values(test_raster_l1) <- c(99, 99, 99, -99)
+# test_raster_l2 <- rast(nrows = nrows, ncols = ncols)
+# values(test_raster_l2) <- c(99, 99, 0, 99)
+# test_raster_l3 <- rast(nrows = nrows, ncols = ncols)
+# values(test_raster_l3) <- c(-99, 99, 0, -99)
+# test_raster_l4 <- rast(nrows = nrows, ncols = ncols)
+# values(test_raster_l4) <- c(99, 99, 99, 99)
+# test_raster_l5 <- rast(nrows = nrows, ncols = ncols)
+# values(test_raster_l5) <- c(99, 99, 99, -99)
+# 
+# test_stack <- c(test_raster_l1, test_raster_l2, test_raster_l3, test_raster_l4, test_raster_l5)
+# test_binned_raster <- app(test_stack, fun=get_heatwave_counts)
+# plot(test_binned_raster) # matches expected: 2, 1, 1, 0
